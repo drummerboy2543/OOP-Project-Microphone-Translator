a108a83ba8f70d371a8ee487fe8a3568a4e0bf0e
drummerboy2543 <att11@zips.uakron.edu>
2017-03-09 00:36:48 -0500

    Added Additional Content

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.SourceList = new System.Windows.Forms.ListView();
+            this.Device = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Channels = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.SourceList);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button5);
+            this.splitContainer1.Panel2.Controls.Add(this.button4);
+            this.splitContainer1.Panel2.Controls.Add(this.button3);
+            this.splitContainer1.Panel2.Controls.Add(this.button2);
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Size = new System.Drawing.Size(508, 261);
+            this.splitContainer1.SplitterDistance = 308;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // SourceList
+            // 
+            this.SourceList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Device,
+            this.Channels});
+            this.SourceList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SourceList.Location = new System.Drawing.Point(0, 0);
+            this.SourceList.MultiSelect = false;
+            this.SourceList.Name = "SourceList";
+            this.SourceList.Size = new System.Drawing.Size(308, 261);
+            this.SourceList.TabIndex = 0;
+            this.SourceList.UseCompatibleStateImageBehavior = false;
+            this.SourceList.View = System.Windows.Forms.View.Details;
+            // 
+            // Device
+            // 
+            this.Device.Text = "Device";
+            this.Device.Width = 160;
+            // 
+            // Channels
+            // 
+            this.Channels.Text = "Channels";
+            this.Channels.Width = 92;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(39, 174);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(128, 33);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "To Wave";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(39, 226);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(128, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(39, 131);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(128, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(39, 76);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Refresh Source";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 261);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Audio Recorder";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView SourceList;
+        private System.Windows.Forms.ColumnHeader Device;
+        private System.Windows.Forms.ColumnHeader Channels;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            List<NAudio.Wave.WaveInCapabilities> sources = new List<NAudio.Wave.WaveInCapabilities>();
+            for (int i = 0; i < NAudio.Wave.WaveIn.DeviceCount; i++)
+            {
+
+                sources.Add(NAudio.Wave.WaveIn.GetCapabilities(i));
+            }
+            SourceList.Items.Clear();
+            foreach (var source in sources) {
+
+                ListViewItem Item = new ListViewItem(source.ProductName);
+                Item.SubItems.Add(new ListViewItem.ListViewSubItem(Item, source.Channels.ToString()));
+                SourceList.Items.Add(Item);
+            }; 
+        }
+
+
+        private NAudio.Wave.WaveIn sourcestream = null;
+        private NAudio.Wave.DirectSoundOut wavout = null;
+        private NAudio.Wave.WaveFileWriter wavewriter = null;
+        private void button2_Click(object sender, EventArgs e)
+        { 
+            if (SourceList.SelectedItems.Count==0) return;
+
+            int deviceNumber = SourceList.SelectedItems[0].Index;
+            sourcestream = new NAudio.Wave.WaveIn();
+            sourcestream.DeviceNumber = deviceNumber;
+            sourcestream.WaveFormat = new NAudio.Wave.WaveFormat(44100, NAudio.Wave.WaveIn.GetCapabilities(deviceNumber).Channels);
+
+            NAudio.Wave.WaveInProvider waveIn = new NAudio.Wave.WaveInProvider(sourcestream);
+            wavout = new NAudio.Wave.DirectSoundOut();
+            wavout.Init(waveIn);
+            sourcestream.StartRecording();
+            wavout.Play();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (SourceList.SelectedItems.Count == 0) return;
+
+            string audio_location = "C:\\Users\\Andrew\\Documents\\Visual Studio 2015\\Projects\\micprogram\\AudioFiles";
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Wave File (*.wav)|*.wav;";
+            if (save.ShowDialog() != System.Windows.Forms.DialogResult.OK) return;
+            int deviceNumber = SourceList.SelectedItems[0].Index;
+            sourcestream = new NAudio.Wave.WaveIn();
+            sourcestream.DeviceNumber = deviceNumber;
+            sourcestream.WaveFormat = new NAudio.Wave.WaveFormat(44100, NAudio.Wave.WaveIn.GetCapabilities(deviceNumber).Channels);
+            sourcestream.DataAvailable += new EventHandler<NAudio.Wave.WaveInEventArgs>(sourcestream_DataAvalible);
+            try
+            {
+                wavewriter = new NAudio.Wave.WaveFileWriter(save.FileName, sourcestream.WaveFormat);
+            }
+            catch {; }
+            sourcestream.StartRecording();
+        }
+        private void sourcestream_DataAvalible(object sender, NAudio.Wave.WaveInEventArgs e)
+        {
+
+
+            if (wavewriter == null) return;
+            wavewriter.WriteData(e.Buffer, 0, e.BytesRecorded);
+            wavewriter.Flush();
+            ;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (wavout != null)
+            {
+
+                wavout.Stop();
+                wavout.Dispose();
+                wavout = null;
+            }
+                if (sourcestream != null) {
+
+                    sourcestream.StopRecording();
+                    sourcestream.Dispose();
+                    sourcestream = null;
+                }
+
+            if (wavewriter != null)
+            {
+                wavewriter.Dispose();
+                wavewriter = null;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button3_Click(sender, e);
+            this.Close();
+        }
+
+        
+    }
+}
+    
